0cc1d06562eb0b844df6764d3e39828608ef32fa
Erik Denis <erikdenisrs97@gmail.com>
2024-01-15 13:07:58 -0300

    test get account api

--- api/accounts.go
+++ api/accounts.go
@@ -49,7 +49,7 @@ func (server *Server) getAccount(ctx *gin.Context) {
 	account, err := server.store.GetAccount(ctx, req.ID)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
 			return
 		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
